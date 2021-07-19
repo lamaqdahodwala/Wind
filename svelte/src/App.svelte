f6d9591f0e9e2167741c819330edb0694d4854bc
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-19 10:39:19 -0400

    added card system

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,8 +1,8 @@
 <script>
     async function get_questions(){
         let resp = await fetch('/api/v1/question')
-        let txt = resp.text
-        return JSON.parse(txt)
+        let txt = resp.json()
+        return txt
     }
     let question_promise = get_questions()
 </script>
@@ -11,6 +11,23 @@
     {#await question_promise}
         <progress class="progress"></progress>
     {:then data} 
-        <p>Done loading!</p>
+        <div class="container">
+            {#each data as i}
+                <div class="card">
+                    <div class="card-header">
+                        <h1 class="card-header-title title">{i.title}</h1>
+                    </div>
+                    <div class="card-content">
+                        <p class="content">
+                            {i.body}
+                        </p>
+                    </div>
+                </div>
+            {/each}
+        </div>
+    {:catch error}
+        <div class="container has-text-centered">
+            <p>{error}</p>
+        </div>
     {/await}
 </main>
