d8c82c8984eeeed91468b964ae50716a300829f9
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-19 17:26:44 -0400

    ok its working

--- svelte/src/Question.svelte
+++ svelte/src/Question.svelte
@@ -1,7 +1,24 @@
 <script>
     export let pk;
+    async function load_question(){
+        let resp = await fetch(`/api/v1/question/${pk}`)
+        let json = resp.json()
+        return json
+    }
 </script>
 
 <main>
-    The pk is {pk}
+    {#await load_question()}
+        <progress class="progress"></progress>
+    {:then data}
+        <div class="container">
+            <div class="box is-fluid">
+                <h1 class="title">{data.title}</h1>
+                <hr>
+                <p class="content">
+                    {data.body}
+                </p>
+            </div>
+        </div>
+    {/await}
 </main>
