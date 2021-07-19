1f5ccdc0e575cbc916c5fd947cbc4192e9767d8c
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-19 10:32:35 -0400

    nice

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,7 +1,16 @@
 <script>
-
+    async function get_questions(){
+        let resp = await fetch('/api/v1/question')
+        let txt = resp.text
+        return JSON.parse(txt)
+    }
+    let question_promise = get_questions()
 </script>
 
 <main>
-    
+    {#await question_promise}
+        <progress class="progress"></progress>
+    {:then data} 
+        <p>Done loading!</p>
+    {/await}
 </main>
