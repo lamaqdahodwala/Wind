afeeea7650866cb7681f783be39d2d0b6328c223
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-19 10:01:51 -0400

    post is working

--- wind/main/api/serializers.py
+++ wind/main/api/serializers.py
@@ -3,7 +3,7 @@ from ..models import Question
 from rest_framework.serializers import  ModelSerializer
 from rest_framework import serializers
 class QuestionSerializer(ModelSerializer):
-    op = serializers.SlugRelatedField(many=False, read_only=True, slug_field='id')
+    op = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
     class Meta:
         model = Question
         fields = ('op', 'title', 'body')
