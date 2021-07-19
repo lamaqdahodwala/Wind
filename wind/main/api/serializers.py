f9bd92f1a56d4b4d29bd99c4aaf55c724675fbd6
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-19 09:07:36 -0400

    wow

--- wind/main/api/serializers.py
+++ wind/main/api/serializers.py
@@ -1,7 +1,14 @@
+from django.contrib.auth.models import User
 from ..models import Question
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import  ModelSerializer
 
 class QuestionSerializer(ModelSerializer):
     class Meta:
         model = Question
-        fields = ('title', 'body')
+        fields = '__all__'
+    
+    def create(self, validated_data):
+        uid = validated_data.pop('op_id', None)
+        user = User.objects.get(pk=uid)
+
+        return Question.objects.create(user=user, **validated_data)
