9ca50e8eb93f10bbf52a1efb204379db8b3aebb4
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-19 09:40:35 -0400

    wow it works

--- wind/main/api/serializers.py
+++ wind/main/api/serializers.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import User
 from ..models import Question
 from rest_framework.serializers import  ModelSerializer
-
+from rest_framework import serializers
 class QuestionSerializer(ModelSerializer):
+    op = serializers.SlugRelatedField(many=False, read_only=True, slug_field='id')
     class Meta:
         model = Question
         fields = ('op', 'title', 'body')
