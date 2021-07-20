c59035544d1a24c7e54ee03e924008db29837ab6
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-20 18:14:29 -0400

    finallyyyyyyyy

--- svelte/src/Question.svelte
+++ svelte/src/Question.svelte
@@ -1,8 +1,13 @@
 <script>
     export let pk;
+    let name;
     async function load_question(){
         let resp = await fetch(`/api/v1/question/${pk}`)
-        let json = resp.json()
+        let json = await resp.json()
+        let get_username = await fetch(json['op'])
+        let d = await get_username.json()
+        name = d['username']
+        
         return json
     }
 </script>
@@ -14,7 +19,7 @@
         <div class="container">
             <div class="box is-fluid">
                 <h1 class="title">{data.title}</h1>
-                <h1 class="subtitle">Written by {data.op.username}</h1>
+                <h1 class="subtitle">Written by {name}</h1>
                 <hr>
                 <p class="content">
                     {data.body}
