741ccff5aae5cca98deeb257cfc54d3195807ab4
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-20 18:27:59 -0400

    added another error message

--- svelte/src/Create_Question.svelte
+++ svelte/src/Create_Question.svelte
@@ -15,8 +15,13 @@
                 "body": body
             })
         })
-        let data = await resp.json()
-        window.location.pathname = `/question/${data.id}`
+        if (resp.ok) {
+            let data = await resp.json()
+            window.location.pathname = `/question/${data.id}`
+            return;
+        }
+        alert("There was an error with your form.")
+
     }
 </script>
 
@@ -25,10 +30,10 @@
         <h1 class="title">New Question</h1>
         <div class="container">
             <input type="text" bind:value={title} placeholder="Epic title..." class="input {!!title ? "" : "is-danger"}">
-            <p class="help">Title {!!title? "| Fill out this field" : ""}</p>
+            <p class="help">Title {!!title? "" : "| Fill out this field"}</p>
             <br>
             <textarea type="text" bind:value={body} placeholder="Epic body" class="textarea {!!body ? "" : "is-danger"}"></textarea>
-            <p class="help">Body {!!body? "| Fill out this field" : ""}</p>
+            <p class="help">Body {!!body? "" : "| Fill out this field"}</p>
             <br>
             <button class="button is-info" on:click={create_question}>Create</button>
         </div>
