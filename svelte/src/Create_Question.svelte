7a5f22a4e9b515a0978ffa7a0edea62a6a493125
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-20 10:15:45 -0400

    created helper function

---
+++ svelte/src/Create_Question.svelte
@@ -0,0 +1,12 @@
+<script>
+
+</script>
+
+<main>
+    <div class="container">
+        <h1 class="title">New Question</h1>
+        <div class="container">
+            <form action="" method="post"></form>
+        </div>
+    </div>
+</main>
