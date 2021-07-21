65f20ce4840bc777b685e266ea07d4fdd9076350
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-21 10:59:53 -0400

    jank should be gone

--- wind/main/api/serializers.py
+++ wind/main/api/serializers.py
@@ -4,20 +4,19 @@ from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 
 
-
-class AnswerSerializer(ModelSerializer):
-    user = serializers.HyperlinkedRelatedField(default=serializers.CurrentUserDefault(), queryset=User.objects.all(), view_name='user-detail')
-    question = serializers.RelatedField()
-
-
-class QuestionSerializer(ModelSerializer):
-    op = serializers.HyperlinkedRelatedField(default=serializers.CurrentUserDefault(), queryset=User.objects.all(), view_name='user-detail')
-    class Meta:
-        model = Question
-        fields = ("op", "title", "body", 'id')
-
-
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ("username",)
+
+class AnswerSerializer(ModelSerializer):
+    user = UserSerializer(default=serializers.CurrentUserDefault())
+    question = serializers.RelatedField()
+
+
+class QuestionSerializer(ModelSerializer):
+    op = UserSerializer(default=serializers.CurrentUserDefault())
+    class Meta:
+        model = Question
+        fields = ("op", "title", "body", 'id')
+
