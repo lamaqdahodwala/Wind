5fba11d128930956dacdda92acd8a6fa9ad2bfd1
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-21 12:26:12 -0400

    edited urls

--- wind/main/api/urls.py
+++ wind/main/api/urls.py
@@ -1,7 +1,10 @@
 from . import views
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r"question", views.QuestionViewSet, basename="question")
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path('answer/<int:qid>/create', views.CreateAnswerView.as_view(), name='createanswer')
+]
