a7f6c538b249d33b2d37bccb209e431e79a1d625
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-21 12:38:56 -0400

    added answer posting mechansim.

--- svelte/src/Question.svelte
+++ svelte/src/Question.svelte
@@ -1,10 +1,33 @@
 <script>
     export let pk;
-    let name;
+    import { getCookie } from './helpers/helpers.js'
+    let answer_body;
     async function load_question(){
         let resp = await fetch(`/api/v1/question/${pk}`)
         let json = await resp.json()
         return json
+    }
+    async function send_answer(){
+        if (!answer_body){
+            alert("Fill out the answer first.")
+            return
+        }
+        let resp = await fetch(`/api/v1/answer/${pk}/create`, {
+            method: "POST",
+            headers: {
+                "X-CSRFToken": getCookie('csrftoken'),
+                "Content-Type": "application/json"
+            },
+            mode: "same-origin",
+            cache: "no-cache",
+            body: JSON.stringify({
+                "body": answer_body
+            })
+        })
+        if (resp.ok){
+            return window.location.reload()
+        }
+        alert("There was an error with your form.")
     }
 </script>
 
@@ -38,6 +61,13 @@
                     <p>No answers yet</p>
                 </div>
             {/each}
+            <h1 class="subtitle is-4">Have an answer?</h1>
+            <div class="control">
+                <textarea cols="30" rows="10" class="textarea" bind:value={answer_body}></textarea>
+                <p class="help">{!!answer_body ? "" : "Fill out this field"}</p>
+                <br>
+                <button class="button is-info is-outlined" on:click={send_answer}>Post answer</button>
+            </div>
         </div>
     {:catch error}
         <p>{error}</p>
