4e01c3d21c7da3d3e7183c6f28202cdf0764d88a
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-21 12:41:07 -0400

    registered answer

--- wind/main/admin.py
+++ wind/main/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Answer
 
 # Register your models here.
 
 admin.site.register(model_or_iterable=Question)
+admin.site.register(Answer)
