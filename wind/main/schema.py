e35cd6e4d600cee50d3834f196053b146768941f
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-23 20:33:12 -0400

    added usertype

--- wind/main/schema.py
+++ wind/main/schema.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 import graphene
 from graphene_django import DjangoObjectType
 from .models import Question, Answer
@@ -5,12 +6,17 @@ from .models import Question, Answer
 class QuestionType(DjangoObjectType):
     class Meta:
         model = Question
-        fields = ('title', 'body', 'user')
+        fields = ('title', 'body', 'user', 'answers')
 
 class AnswerType(DjangoObjectType):
     class Meta:
         model = Answer
         fields = ('user', 'content')
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+        fields = ('username',)
 
 class Query(graphene.ObjectType):
     all_questions = graphene.List(QuestionType)
