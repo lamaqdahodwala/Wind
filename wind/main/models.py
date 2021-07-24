28b84ef44d2cd85a53ab843c0915a9140b3cc308
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-23 20:22:20 -0400

    stuff

--- wind/main/models.py
+++ wind/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='questions')
+
+class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='answers')
+    content = models.TextField()
