037222e36445bac9d8b41f8ee7b3d7ddffd32aaf
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-24 10:53:47 -0400

    edited schema

--- wind/main/schema.py
+++ wind/main/schema.py
@@ -21,11 +21,14 @@ class UserType(DjangoObjectType):
 class Query(graphene.ObjectType):
     all_questions = graphene.List(QuestionType)
     single_question = graphene.Field(QuestionType, pk=graphene.Int())
-
+    user_by_id = graphene.Field(UserType, id=graphene.Int())
     def resolve_all_questions(root, info):
         return Question.objects.all()
 
     def resolve_single_question(root, info, pk):
         return Question.objects.get(pk=pk)
+    
+    def resolve_user_by_id(root, info, id):
+        return User.objects.get(pk=id)
 
 schema = graphene.Schema(query=Query)
