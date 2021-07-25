20e8a39d4eb64cfb6b5094d27ab91c8eedd911c2
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-25 18:12:44 -0400

    added new query

--- frontend/src/Question.svelte
+++ frontend/src/Question.svelte
@@ -1,3 +1,15 @@
 <script>
+    import { send_gql_request } from './assets/assets.js'
+    import { queries } from './assets/queries.js'
+
     export let pk
+
+    async function load_question(){
+        let resp = await send_gql_request()
+        return resp['data']['singleQuestion']
+    }
 </script>
+
+<p>
+    The pk is {pk}
+</p>
--- frontend/src/assets/queries.js
+++ frontend/src/assets/queries.js
@@ -1,3 +1,4 @@
 export let queries = {
-    all_questions: "{\n  allQuestions{\n    title\n    body\n    id \n}\n}"
+    all_questions: "{\n  allQuestions{\n    title\n    body\n    id \n}\n}",
+    single_question: "query GetSingleQuestion($pk:Int){\n  singleQuestion(pk:$pk){\n    title\n    body\n    user{\n      username\n    }\n  }\n}"
 }
