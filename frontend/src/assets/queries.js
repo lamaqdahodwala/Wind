2dce940fd36ef3e82e7394eaa307686721dd18f1
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-26 11:38:29 -0400

    edited query

--- frontend/src/assets/queries.js
+++ frontend/src/assets/queries.js
@@ -1,4 +1,4 @@
 export let queries = {
     all_questions: "{\n  allQuestions{\n    title\n    body\n    id \n}\n}",
-    single_question: "query GetSingleQuestion($pk:Int){\n  singleQuestion(pk:$pk){\n    title\n    body\n    user{\n      username\n    }\n    answers{\n      content\n      user{\n        username\n        id\n        }\n    }\n  }\n}"
+    single_question: "query GetSingleQuestion($pk:Int){\n  singleQuestion(pk:$pk){\n    title\n    body\n    user{\n      username\n    }\n    answers{\n      content\n      user{\n        username\n        id\n      }\n    }\n  }\n}"
 }
